3006377ff642ccfaa608dbdd65f0e985c38891ee
Vahid Fardi <vahidfardi2011@gmail.com>
2018-01-13 18:14:34 +0330

    add some change

---
+++ src/app.c
@@ -0,0 +1,8 @@
+#include "lanNetwork.h"
+
+void main ()
+{
+//      lanServer(8000);
+        lanClient(8000,"vahid fardi");
+}
+
---
+++ src/connections/lanNetwork.h
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8080
+
+
+
+int lanClient(int port , char *message);
+int lanServer(int port);
+
